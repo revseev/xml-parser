97a0edba2b9be71a945d4720c436277302460e34
Roman Evseev <useless47@gmail.com>
2020-08-06 22:12:06 +0300

    changes to XML file to make it work, and also corrected logic error in parsePage()

--- src/main/java/com/revseev/xml_parser/WikipediaDataParser.java
+++ src/main/java/com/revseev/xml_parser/WikipediaDataParser.java
@@ -1,17 +1,16 @@
 package com.revseev.xml_parser;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.List;
-import java.util.zip.ZipInputStream;
-
 import javax.xml.stream.XMLEventReader;
 import javax.xml.stream.XMLInputFactory;
 import javax.xml.stream.XMLStreamException;
 import javax.xml.stream.events.StartElement;
 import javax.xml.stream.events.XMLEvent;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
 
 public class WikipediaDataParser {
+
     private static final String ELEMENT_PAGE = "page";
     private static final String ELEMENT_NAME = "name";
     private static final String ELEMENT_ID = "id";
@@ -26,15 +25,13 @@ public class WikipediaDataParser {
     }
 
     public void parse() throws IOException, XMLStreamException {
-        try(final InputStream stream = this.getClass().getResourceAsStream(file)) {
-            try(final ZipInputStream zip = new ZipInputStream(stream)) {
-                final XMLEventReader reader = factory.createXMLEventReader(zip);
-                while (reader.hasNext()) {
-                    final XMLEvent event = reader.nextEvent();
-                    if (event.isStartElement() && event.asStartElement().getName()
-                                                       .getLocalPart().equals(ELEMENT_PAGE)) {
-                        parsePage(reader);
-                    }
+        try (final InputStream stream = this.getClass().getResourceAsStream(file)) {
+            final XMLEventReader reader = factory.createXMLEventReader(stream);
+            while (reader.hasNext()) {
+                final XMLEvent event = reader.nextEvent();
+                if (event.isStartElement() && event.asStartElement().getName()
+                                                   .getLocalPart().equals(ELEMENT_PAGE)) {
+                    parsePage(reader);
                 }
             }
         }
@@ -46,7 +43,7 @@ public class WikipediaDataParser {
         while (reader.hasNext()) {
             final XMLEvent event = reader.nextEvent();
             if (event.isEndElement() && event.asEndElement().getName().getLocalPart().equals(ELEMENT_PAGE)) {
-                return;
+                break;
             }
             if (event.isStartElement()) {
                 final StartElement element = event.asStartElement();
--- src/test/java/com/revseev/xml_parser/WikipediaDataParserTest.java
+++ src/test/java/com/revseev/xml_parser/WikipediaDataParserTest.java
@@ -15,10 +15,12 @@ public class WikipediaDataParserTest {
     public void testParse() throws IOException, XMLStreamException {
         final List<WikipediaPage> pages = new ArrayList();
         final WikipediaDataParser parser = new
-                WikipediaDataParser("/testwiki-20170620-pages-articles-multistream.xml.zip",
+                WikipediaDataParser("/metawiki-20191120-pages-articles-multistream2.xml-p1821603p2208725.xml",
                 pages
         );
         parser.parse();
-        assertTrue(pages.size() > 0);
+        int size = pages.size();
+        System.out.println(size);
+        assertTrue(size > 0);
     }
 }
